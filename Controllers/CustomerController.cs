a0f88e4c2ffc5b23f7bb582b9613f4cd37337218
Paul Leavitt <paul.leavitt@1ict.com.au>
2019-08-30 16:39:37 +1000

    first commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace webapplication.Controllers
+{ 
+
+[Route("api/[controller]")]
+[ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet, Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "John Doe", "Jane Doe" };
+        }
+
+    }
+}
